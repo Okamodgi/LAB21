bb66834efe1e0ab4c718590e05fc3858bbdfb749
okamodgi <nadakupnada@gmail.com>
2023-06-22 17:59:04 +0300

    laboratornaya21

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
+
+interface Observer {
+    void update(String message);
+}
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Group gr1 = new Group("ОНИ");
+        Group gr2 = new Group("НЛО");
+        Group gr3 = new Group("Мы");
+        Group gr4 = new Group("Время");
+
+        User us1 = new User("us 1");
+        User us2 = new User("us 2");
+
+        gr1.addObserver(us1);
+        gr1.addObserver(us2);
+        gr2.addObserver(us1);
+        gr2.addObserver(us2);
+        gr3.addObserver(us1);
+        gr4.addObserver(us2);
+
+        gr1.sendNotification("Новый запись в сообществе");
+        gr2.sendNotification("в Краснодаре замечен....");
+        gr3.sendNotification("Гороскоп на 23 мая");
+        gr4.sendNotification("Новости дня");
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class User {
+public class User implements Observer {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+    public void update(String message) {
+        System.out.println("Пользователь " + name + " получил оповещение: " + message);
+    }
 }
